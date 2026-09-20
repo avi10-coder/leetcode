5ef237ec99b8c507ccf78021aa34b3dbaee7e6f8
avi10-coder <avikhandelwal10@gmail.com>
2026-09-21 01:25:24 +0530

    Time: 7 ms (3.94%) | Memory: 20.9 MB (75.42%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -15,16 +15,14 @@ class Solution:
         valid_left, left = self.inorder(root.left, result)
         val = [root.val]
         valid_right, right = self.inorder(root.right, result)
-        if (left and left[-1] == val[0]) or (right and right[0] == val[0]) or (not valid_left) or (not valid_right):
+        if (left and left[-1] >= val[0]) or (right and right[0] <= val[0]) or (not valid_left) or (not valid_right):
             valid = False
         out = left + val + right
         return valid, out
 
     def isValidBST(self, root: TreeNode | None) -> bool:
         valid, result = self.inorder(root, [])
-        sorted_result = result.copy()
-        sorted_result.sort()
-        return valid and sorted_result == result
+        return valid
         
         
         
